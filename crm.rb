cdb929b22303e65e3ff7911d4d9207b59eb40244
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-27 15:28:24 -0500

    Changed contacts.erb file by updating it so it can use rolodex as well.

--- crm.rb
+++ crm.rb
@@ -2,17 +2,16 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
+$rolodex= Rolodex.new
+
+
+#different routs 
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-	
 	erb :contacts 
 end 
 
