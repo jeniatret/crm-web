3a7777c083ca92a4e76b42b2295fd99ba9c2e828
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-29 14:49:33 -0500

    created 404 page for the contact that does not exist

--- crm.rb
+++ crm.rb
@@ -21,7 +21,11 @@ end
 
 get "/contacts/:id" do
   @contact = @@rolodex.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get '/contacts/new' do
