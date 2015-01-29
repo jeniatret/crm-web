5f923f229f0ff3af288b3d5d587327c0a73ef701
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-29 15:58:13 -0500

    Created a new route that handles a put request. Also added a mistake message if the contact is not found and if the contact is found

--- crm.rb
+++ crm.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
-$rolodex= Rolodex.new
 @@rolodex = Rolodex.new
 @@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 @@rolodex.add_contact(Contact.new("Jenia", "Tretiyakova", "zhanyat@hotmail.com", "student"))
@@ -28,6 +27,21 @@ get "/contacts/:id" do
   end
 end
 
+put "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
 get "/contacts/:id/edit" do
   @contact = @@rolodex.find(params[:id].to_i)
   if @contact
@@ -43,6 +57,6 @@ end
 
 post '/contacts' do
  	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  	$rolodex.add_contact(new_contact)
+  	@@rolodex.add_contact(new_contact)
   	redirect to('/')
 end
