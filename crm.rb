580639f44b1d7a8d23565a6c1e4818fea2dbda94
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-29 15:26:26 -0500

    Created a put method here. New path edit and was able acces edit page in google chrome.

--- crm.rb
+++ crm.rb
@@ -28,6 +28,15 @@ get "/contacts/:id" do
   end
 end
 
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/contacts/new' do
 	erb :new_contact
 end
